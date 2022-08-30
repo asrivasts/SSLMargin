0f5f0e2c8bc21bef229c6ab838f2470807178d75
asrivasts <asrivasts@gmail.com>
2022-08-30 13:13:54 -0400

    Version 1 of app

---
+++ static/js/react.js
@@ -0,0 +1,51 @@
+document.getElementById("shicost").addEventListener("change", myFunction);
+document.getElementById("custcost").addEventListener("change", myFunction2);
+var shi = 0;
+var cust = 0;
+
+function myFunction() {
+  var x = document.getElementById("shicost");
+  if (x.value =="" || x.value == null){
+      shi=0;
+  }
+  else{
+    x.value = x.value.replace('$','');
+    x.value =x.value.trim();
+    shi=1
+  }
+  marginCalc();
+}
+
+function myFunction2() {
+  var x = document.getElementById("custcost");
+  if (x.value =="" || x.value == null){
+    cust=0;
+  }
+  else{
+    x.value = x.value.replace('$','');
+    x.value =x.value.trim();
+    cust=1
+  }
+  marginCalc();
+}
+
+function marginCalc(){
+    if (shi == 1 && cust ==1){
+        var m = document.getElementById("calcMargin");
+        var s = document.getElementById("shicost").value;
+        var c = document.getElementById("custcost").value;
+        var marg = 0
+        s = parseFloat(s.replace(/,/g,''));
+        c = parseFloat(c.replace(/,/g,''));
+        marg = 100*(c-s)/c;
+        m.innerHTML="<h1>SSL Margin: " + parseFloat(marg).toFixed(2) + "%</h1>"
+        // (shi)/(1-%) = cust
+        // shi = cust - % * cust
+        // % * cust = cust - shi
+        // % = (cust - shi)/cust
+    }
+    else{
+        var m = document.getElementById("calcMargin");
+        m.innerHTML="";
+    }
+}
